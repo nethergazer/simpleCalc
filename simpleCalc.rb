cb40293d58ce6503e32e3b8cd308e19bccb511ea
Jacob Legg <skeletalsoup@protonmail.com>
2018-06-12 15:10:49 -0400

    Updated calc

--- simpleCalc.rb
+++ simpleCalc.rb
@@ -2,13 +2,13 @@
 
 
 #'opening text on startup'
-p "Which form of math would you like to use Add[1] subtrack[2] divide [3] multiply[4]-input corrosponding number"
+p "Which form of math would you like to use Add[1] subtract[2] divide [3] multiply[4]-input corrosponding number"
 formula_selection = gets.chomp
 
 #'First if statment that handles addition'
-case formula_selection
-
-when "1"
+1.times do formula_selection
+p "formula_selection is #{formula_selection.class}"
+if formula_selection == "1"
 p "You are currently doing addition"
 p "enter your first value"
 #'assigns first value to be used using a variable'
@@ -23,39 +23,44 @@ p "Total equals"
 p val_total
 
 #'continued if statment that handles subtractions'
-when "2" 
+elsif formula_selection == "2"
     p "You are currently doing subtraction"
     p "enter your first value"
     val_A = gets.to_i 
     p "enter your second value"
-    val_B = gets.chomp
+    val_B = gets.to_i
     val_total = val_A.to_i - val_B.to_i
     p "Total equals"
     p val_total
 
 #'continued if statement that handles division'
-when "3" 
+elsif formula_selection == "3"
     p "You are currently doing division"
     p "enter your first value"
-    val_A = gets.chomp
+    val_A = gets.to_i
     p "enter your second value"
-    val_b = gets.chomp
+    val_B = gets.to_i
     val_total = val_A.to_i / val_B.to_i
+if formula_selection == "3" && val_B == 0
+    p "Enter another value aside from 0"
+end
     p "total equals"
     p val_total
 
+
 #'Final statement that closes with the else function'
-when "4"
+else formula_selection == "4"
     p "you are currently doing multiplication"
     p "enter your first value"
-    val_A = gets.chomp
+    val_A = gets.to_i
     p "enter your second value"
-    val_B = gets.chomp
+    val_B = gets.to_i
     val_total = val_A.to_i * val_B.to_i
     p "total equals"
     p val_total 
 end
 
+end
 
 
 
