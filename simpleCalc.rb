34730cda31047daf2d8fd93a326f4a0428831783
Jacob Legg <skeletalsoup@protonmail.com>
2018-06-12 17:11:05 -0400

    updated zero division

--- simpleCalc.rb
+++ simpleCalc.rb
@@ -1,28 +1,21 @@
 
-
-
-#'opening text on startup'
 p "Which form of math would you like to use Add[1] subtract[2] divide [3] multiply[4]-input corrosponding number"
 formula_selection = gets.chomp
 
-#'First if statment that handles addition'
+
 1.times do formula_selection
 p "formula_selection is #{formula_selection.class}"
 if formula_selection == "1"
 p "You are currently doing addition"
 p "enter your first value"
-#'assigns first value to be used using a variable'
+
 val_A = gets.to_i 
 p "enter your second value"
-#'assigns second value to be used using a variable'
 val_B = gets.to_i 
-#'Does math for two assigned variables and outputs them to a total'
 val_total = val_A.to_i + val_B.to_i
 p "Total equals"
-#'displays answer'
 p val_total
 
-#'continued if statment that handles subtractions'
 elsif formula_selection == "2"
     p "You are currently doing subtraction"
     p "enter your first value"
@@ -33,22 +26,29 @@ elsif formula_selection == "2"
     p "Total equals"
     p val_total
 
-#'continued if statement that handles division'
+
 elsif formula_selection == "3"
+    
     p "You are currently doing division"
     p "enter your first value"
     val_A = gets.to_i
     p "enter your second value"
     val_B = gets.to_i
-    val_total = val_A.to_i / val_B.to_i
-if formula_selection == "3" && val_B == 0
-    p "Enter another value aside from 0"
+    if formula_selection == "3" && val_B.to_i == 0
+        p "Enter another value aside from 0"
+    end
+    break 
+    
 end
+
+
+    val_total = val_A.to_i / val_B.to_i
+
     p "total equals"
     p val_total
 
 
-#'Final statement that closes with the else function'
+
 else formula_selection == "4"
     p "you are currently doing multiplication"
     p "enter your first value"
