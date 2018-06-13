f50d746e00fa047d65939df1753f101d5233afbf
Jacob Legg <skeletalsoup@protonmail.com>
2018-06-13 13:12:09 -0400

    Cleaned up code

--- simpleCalc.rb
+++ simpleCalc.rb
@@ -1,58 +1,41 @@
+puts "Which form of math would you like to use Add[1] subtract[2] divide [3] multiply[4]-input corrosponding number"
 
-p "Which form of math would you like to use Add[1] subtract[2] divide [3] multiply[4]-input corrosponding number"
-formula_selection = gets.chomp
+operation = gets.chomp
+1.times do operation
+"operation is #{operation.class}"
 
-
-1.times do formula_selection
-p "formula_selection is #{formula_selection.class}"
-if formula_selection == "1"
-p "You are currently doing addition"
-p "enter your first value"
+if operation == "1"
+puts "You are currently doing addition, enter two values"
 val_A = gets.to_i 
-p "enter your second value"
 val_B = gets.to_i 
 val_total = val_A.to_i + val_B.to_i
-p "Total equals"
-p val_total
+puts "Total = #{val_total}"
 
-elsif formula_selection == "2"
-    p "You are currently doing subtraction"
-    p "enter your first value"
+elsif operation == "2"
+    puts "You are currently doing subtraction, enter two values"
     val_A = gets.to_i 
-    p "enter your second value"
     val_B = gets.to_i
     val_total = val_A.to_i - val_B.to_i
-    p "Total equals"
-    p val_total
+    puts "Total = #{val_total}"
 
-
-elsif formula_selection == "3"
-    
-    p "You are currently doing division"
-    p "enter your first value"
+elsif operation == "3"
+    puts "You are currently doing division, enter two values"
     val_A = gets.to_i
-    p "enter your second value"
     val_B = gets.to_i
-    if formula_selection == "3" && val_B.to_i == 0
-        p "Enter another value aside from 0"
+    if operation == "3" && val_B.to_i == 0
+        puts "Enter another value aside from 0"
         break
     end
     val_total = val_A.to_i / val_B.to_i
-    p "total equals"
-    p val_total
+    puts "Total = #{val_total}"
 
-else formula_selection == "4"
-    p "you are currently doing multiplication"
-    p "enter your first value"
+else operation == "4"
+    puts "You are currently doing multiplication, enter two values"
     val_A = gets.to_i
-    p "enter your second value"
     val_B = gets.to_i
     val_total = val_A.to_i * val_B.to_i
-    p "total equals"
-    p val_total 
-
-
+    puts "Total = #{val_total}"
 end
 
-end
+        end
 
